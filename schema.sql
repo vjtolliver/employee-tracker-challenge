752e8ff4945a0a4cbfa1627bd22baf93336978cc
vjtolliver <vj.tolliver@gmail.com>
2023-04-29 15:48:53 -0700

    added initial files and packages

---
+++ schema.sql
@@ -0,0 +1,16 @@
+-- Departments Table
+    -- Department ID : INT PRIMARY KEY
+    -- Departments Name : VARCHAR(30)
+
+-- Roles Table
+    -- Role ID : INT PRIMARY KEY
+    -- Job Title : VARCHAR(30)
+    -- Salary : DECIMAL
+    -- Department ID : INT FORIEGN KEY
+
+-- Employees Table
+    -- Employee ID : INT PRIMARY KEY
+    -- first name : VARCHAR(30)
+    -- last name : VARCHAR(30)
+    -- Role ID : INT FOREIGN KEY
+    -- Manager ID : INT FOREIGN KEY
